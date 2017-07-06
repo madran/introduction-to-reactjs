0bf83bf18b7b40436c6282383bb1435e17c4f2ed
Łukasz Różański <lukasz.rozanski@gmail.com>
2017-07-06 17:57:38 +0200

    module-02 class components quiz - poprawki + wyświetlanie odpowiedzi w pętli

--- module-02/public_html/js/Answers.js
+++ module-02/public_html/js/Answers.js
@@ -1,19 +1,19 @@
 class Answers extends React.Component {
     render() {
+        var answers = [];
+        for(var i = 0; i < 4; i++) {
+            answers.push(
+                <div>
+                    <label for="answer">{this.props.answers.answers[i]}</label>
+                    <input type="radio" name="answer" value="{i}" /><br />
+                </div>
+            );
+        }
+        
         return (
-            <div>
-                <label for="question-0">{this.props.answers.answers[0]}</label>
-                <input type="radio" value="{this.props.answers.correctAnser[0]}" /><br />
-                
-                <label for="question-1">{this.props.answers.answers[1]}</label>
-                <input type="radio" value="{this.props.answers.correctAnser[1]}" /><br />
-                
-                <label for="question-2">{this.props.answers.answers[2]}</label>
-                <input type="radio" value="{this.props.answers.correctAnser[3]}" /><br />
-                
-                <label for="question-3">{this.props.answers.answers[3]}</label>
-                <input type="radio" value="{this.props.answers.correctAnser[3]}" /><br />
-            </div>
+            <form action="#">
+                {answers}
+            </form>
         )
     }
 }
