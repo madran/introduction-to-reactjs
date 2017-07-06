d26b720e79dcfcb53f606ba33e1d33df9d1befff
Łukasz Różański <lukasz.rozanski@gmail.com>
2017-07-06 17:43:55 +0200

    module-02 class components quiz - usuniecie piątej odpowiedzi

--- module-02/public_html/js/Answers.js
+++ module-02/public_html/js/Answers.js
@@ -2,20 +2,17 @@ class Answers extends React.Component {
     render() {
         return (
             <div>
-                <input type="radio" value="{this.props.answers.correctAnser[0]}" /><br />
                 <label for="question-0">{this.props.answers.answers[0]}</label>
+                <input type="radio" value="{this.props.answers.correctAnser[0]}" /><br />
                 
-                <input type="radio" value="{this.props.answers.correctAnser[1]}" /><br />
                 <label for="question-1">{this.props.answers.answers[1]}</label>
+                <input type="radio" value="{this.props.answers.correctAnser[1]}" /><br />
                 
-                <input type="radio" value="{this.props.answers.correctAnser[3]}" /><br />
                 <label for="question-2">{this.props.answers.answers[2]}</label>
-                
                 <input type="radio" value="{this.props.answers.correctAnser[3]}" /><br />
-                <label for="question-3">{this.props.answers.answers[3]}</label>
                 
-                <input type="radio" value="{this.props.answers.correctAnser[4]}" />
-                <label for="question-4">{this.props.answers.answers[4]}</label>
+                <label for="question-3">{this.props.answers.answers[3]}</label>
+                <input type="radio" value="{this.props.answers.correctAnser[3]}" /><br />
             </div>
         )
     }
