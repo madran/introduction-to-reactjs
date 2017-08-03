df52d57f52fb4d650429926c38ffca3def392c90
Łukasz Różański <lukasz.rozanski@gmail.com>
2017-08-03 17:31:51 +0200

    module-03 crs - InputCheckboxList style

--- module-03/public_html/js/course-registration-system/InputCheckboxList.js
+++ module-03/public_html/js/course-registration-system/InputCheckboxList.js
@@ -1,16 +1,23 @@
 function InputCheckboxList(props) {
+    var listCSS = {
+        margin: 0,
+        padding: 0,
+        listStyleType: 'none'
+    }
+    
     var list = props.list.map((item) => {
         return (
             <li>
-                <label for={item.name}>{item.label}</label>
                 <input type="checkbox" name={item.name} value={item.value} />
+                <label for={item.name}>{item.label}</label>
             </li>
         );
     });
     
     return (
         <div>
-            <ul>
+            <div>{props.label}:</div>
+            <ul style={listCSS}>
                 {list}
             </ul>
         </div>
